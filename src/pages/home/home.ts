67419c62c29ec789620efe3843062130f60a0157
tiffanyfl <tiffanyf.levacher@gmail.com>
2018-06-06 15:57:34 +0200

    Add buttons

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -17,7 +17,7 @@ export class HomePage {
     'american-football', 'boat', 'bluetooth', 'build'];
 
     this.items = [];
-    for(let i = 1; i < 11; i++) {
+    for(let i = 1; i < 8; i++) {
       this.items.push({
         title: 'Item ' + i,
         note: 'This is item #' + i,
